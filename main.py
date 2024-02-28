4cea2cb6a2384e9152e33eed876d005735048f19
Chuan Yan <nauhcnay@gmail.com>
2024-02-28 17:21:45 -0500

    bug fix

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # main function entrance
 import eel 
 import os
+import requests
 from random import randint
 from tools.files import open_psd
 
-# need to consider multi-user case? no, let just consider single user case for now
-# I think in the future, we should run the front end locally and backend on cluster
-# but now it is fine to just run everything on the cluster
+PATH_TO_INPUT = './InputImages/'
+PATH_TO_SHADOW = './web/Shadows'
+PATH_TO_SHADOWS = './web/Shadows/sub_shadows'
 
 @eel.expose	 
 def random_python(): 
@@ -15,16 +16,14 @@ def random_python():
     labels_path = "D:/Studies/Eel/EEL-DEMO/yolov5/runs/predict-seg/exp/labels"
     os.system(f'python "{python_file_path}" "{input_images_path}" "{labels_path}"')
 
-# helper function for openning psd files
 @eel.expose
 def open_psd_py(path_to_psd):
-    open_psd(path_to_psd)
-    
+    open_psd(path_to_psd, PATH_TO_INPUT)
+    # 
 
 '''
 for debug
 '''
-# open_psd("./test/image59.psd", "./InputImages/")
 
 # start main GUI
 eel.init("web") 
