57e06e7ae137e24647d352afd870ec4d6bef80a5
Chuan Yan <nauhcnay@gmail.com>
2024-03-15 03:36:20 -0400

    bug fix

--- main.py
+++ main.py
@@ -406,6 +406,6 @@ if __name__ == "__main__":
     # start main GUI
     eel.init("web")
     # let's run this code remotely for now
-    print("log:\tconnecting backend with port:%d"&int(args.port_to_backend))
+    print("log:\tconnecting backend with port:%d"%int(args.port_to_backend))
     print("log:\tOpen a web browser to: http://http://164.90.158.133:%d/GUI2.html"&(8081+int(args.port_to_user)-7001))
     eel.start("GUI2.html", mode=False, all_interfaces=True, size = (1400, 800), port = int(args.port_to_user))
